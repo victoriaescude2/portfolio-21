8d15721be19a65b932693ba5107fa2715d5b7525
ESCUDERO, VICTORIA <victoriaescudero_99@hotmail.com>
2021-10-23 19:36:18 -0300

    Actualizacion Navbar

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import "./About.css"; 
-
 import imagenPerfil from "../../media/imagenPerfil.jpg";
 
 const About = () => {
@@ -8,9 +7,10 @@ const About = () => {
 		<div className="about-container">
 			<div className="about-desc">
 				<h3> </h3>
-				<p>Me llamo Victoria Escudero tengo 21 años. Nací el 29 de Agosto de 1999 en Ayacucho 
-				Provincia de Buenos Aires. Soy estudiante de Licenciatura en Sistemas en la Facultad de Informatica 
-				de la Universidad Nacional de La Plata.</p>
+				<p>Tengo 22 años. Nací el 29 de Agosto de 1999 en Ayacucho, 
+				Provincia de Buenos Aires. Desde el 2018, soy estudiante de Licenciatura en Sistemas en la Facultad de Informática 
+				de la Universidad Nacional de La Plata, actualmente cursando 4to año. También, me encuentro trabajando en la Contaduria de la 
+				Provincia de Buenos Aires y en algunos proyectos personales.</p>
 		    </div>
 		    <div className="about-img">
 				<img src={imagenPerfil} alt="description"/>
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,14 +2,23 @@ import React from 'react'
 import "./Navbar.css";
 
 const Navbar =({isScrolling}) => {
-	const toTheTop = () => {
-		window.scrollTo({ top: 0, left: 0, behaviour: "smooth"});
+	const toAbout = () => {
+		window.scrollTo({ top: 750, left: 0, behaviour: "smooth"});
 	}
+	const toProyects = () => {
+		window.scrollTo({ top: 1400, left: 0, behaviour: "smooth"});
+	}
+	const toContact = () => {
+		window.scrollTo({ top: 2200, left: 0, behaviour: "smooth"});
+	}
+
 	return (
-		<nav className={`navbar ${isScrolling > 10 ? "scrolling" : null}`}>
-			<div className="navbar-logo" onClick={toTheTop}>
-				About me
-			</div>
+		<nav className={`navbar ${isScrolling >= 0 ? "scrolling" : null}`}>
+			<li class="nav-item ms-auto" >
+					<a className="navbar-logo" onClick={toAbout}> Información |</a>
+					<a className="navbar-logo" onClick={toProyects}> Proyectos |</a>
+					<a className="navbar-logo" onClick={toContact}> Contacto |</a>
+			</li>
 		</nav>
 
 	);
